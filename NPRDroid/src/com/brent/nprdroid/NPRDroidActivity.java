67391decd074344959831458a85ded8396a0fd9c
Brent <hgross4@uic.edu>
2013-03-27 20:10:03 -0500

    List and play stories in app.

--- NPRDroid/src/com/brent/nprdroid/NPRDroidActivity.java
+++ NPRDroid/src/com/brent/nprdroid/NPRDroidActivity.java
@@ -2,29 +2,44 @@ package com.brent.nprdroid;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class NPRDroidActivity extends Activity {
-
-	String showChoice, URL;
-	RadioGroup radioGroupShows;
+public class NPRDroidActivity extends ListActivity {
+	private String sdPath; 
+	private List<String> songs = new ArrayList<String>();
+	private MediaPlayer mp = new MediaPlayer();
+	private int currentPosition = 0;
+	private String showChoice;
+	private RadioGroup radioGroupShows;
+	private String TAG = "NPRDroidActivity";
 
 	/** Called when the activity is first created. */
 	@Override
@@ -32,13 +47,55 @@ public class NPRDroidActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		radioGroupShows = (RadioGroup) findViewById(R.id.radioGroupShows);
-		//		The below lines commented because the button's onclick is specified in main.xml now
-		//		Button buttonDownload = (Button) findViewById(R.id.buttonDownload);
-		//		buttonDownload.setOnClickListener(new OnClickListener() {
-		//			public void onClick(View v) {
-		//				readWebpage(v);
-		//			}
-		//		});
+		updateSongList();
+	}
+
+	private void updateSongList() {
+		sdPath = getExternalFilesDir(null).getAbsolutePath();
+		Log.i(TAG , sdPath);
+		File sdPathFile = new File(sdPath);
+		File[] files = sdPathFile.listFiles();
+		if (files.length > 0) {
+			for (File file : files) {
+				songs.add(file.getName());
+			}
+			ArrayAdapter<String> songList = new ArrayAdapter<String>(this, R.layout.song_item, songs);
+			setListAdapter(songList);
+		}
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		currentPosition = position;
+		playSong(sdPath + "/" + songs.get(position));
+	}
+
+	private void playSong(String songPath) {
+		Log.i(TAG, songPath);
+		try {
+			mp.reset();
+			mp.setDataSource(songPath);
+			mp.prepare();
+			mp.start();
+			// Setup listener so next song starts automatically
+			mp.setOnCompletionListener(new OnCompletionListener() {
+				public void onCompletion(MediaPlayer arg0) {
+					nextSong();
+				}
+			});
+		} catch (IOException e) {
+			Log.v(getString(R.string.app_name), e.getMessage());
+		}
+	}
+
+	private void nextSong() {
+		if (++currentPosition >= songs.size()) {
+			// Last song, just reset currentPosition
+			currentPosition = 0;
+		} else {
+			// Play next song
+			playSong(sdPath + songs.get(currentPosition));
+		}
 	}
 
 	private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
@@ -53,18 +110,25 @@ public class NPRDroidActivity extends Activity {
 					public void run() {
 						TextView urlText = (TextView) findViewById(R.id.urlText);
 						urlText.setMovementMethod(new ScrollingMovementMethod());
-						urlText.setText(urlText.getText() + urlCopy + "\n\n");
+						urlText.setText(urlCopy);
 					}
 				});						
 				try {
 					HttpResponse execute = client.execute(httpGet);
 					InputStream content = execute.getEntity().getContent();
 					byte[] buffer = new byte[1024];
-				    int length;
-					FileOutputStream out = new FileOutputStream(new File(getExternalFilesDir(null), index + ".mp3"));
+					int length;
+					File audioFile = new File(getExternalFilesDir(null), index + ".mp3");
+					FileOutputStream out = new FileOutputStream(audioFile);
 					while ((length = content.read(buffer)) > 0) {
 						out.write(buffer, 0, length);
 					}
+					//					LinearLayout layout = (LinearLayout) findViewById(R.id.mainLayout);
+					//				    TextView fileText = new TextView(NPRDroidActivity.this);
+					//				    fileText.setText(index + ": " + (new Date(audioFile.lastModified())).toString());
+					//				    fileText.setId(index);
+					//				    fileText.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+					//				    ((LinearLayout) layout).addView(fileText);
 					++index;
 				} catch (Exception e) {
 					e.printStackTrace();

ad0c4faf21d4fc33e3096fe047b91d553eb1efbf
Brent <hgross4@uic.edu>
2015-03-01 22:56:43 -0600

    Make title bar show and change depending on which fragment is visible; other minor stylistic changes

--- ConstactsList/app/src/main/java/com/brent/constactslist/controllers/MainActivity.java
+++ ConstactsList/app/src/main/java/com/brent/constactslist/controllers/MainActivity.java
@@ -3,6 +3,7 @@ package com.brent.constactslist.controllers;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.SlidingPaneLayout;
+import android.view.View;
 
 import com.brent.constactslist.R;
 import com.brent.constactslist.controllers.ContactsListFragment;
@@ -23,6 +24,22 @@ public class MainActivity extends FragmentActivity implements ContactsListFragme
         detailsFragment =
                 (DetailsFragment)getSupportFragmentManager().findFragmentById(R.id.details);
         pane = (SlidingPaneLayout)findViewById(R.id.pane);
+        pane.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+
+            }
+
+            @Override
+            public void onPanelOpened(View panel) {
+                MainActivity.this.setTitle(R.string.app_name);
+            }
+
+            @Override
+            public void onPanelClosed(View panel) {
+                MainActivity.this.setTitle(getString(R.string.contact_details));
+            }
+        });
         pane.openPane();
     }
 
